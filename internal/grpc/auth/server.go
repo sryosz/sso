26627fb40db9593253e3d1a5a16786eec4f8728b
srymad <zasd4478@gmail.com>
2024-07-10 22:47:49 +0300

    server implementation

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -2,35 +2,86 @@ package auth
 
 import (
 	"context"
+	"github.com/go-playground/validator/v10"
 	ssov1 "github.com/sryosz/protos/gen/go/sso"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+type Auth interface {
+	Login(ctx context.Context,
+		email string,
+		password string,
+		appID int,
+	) (string, error)
+	RegisterNewUser(ctx context.Context,
+		email string,
+		password string,
+	) (int64, error)
+	IsAdmin(ctx context.Context, userID int64) (bool, error)
+}
 
 type serverAPI struct {
 	ssov1.UnimplementedAuthServer
+	auth Auth
 }
 
-func Register(gRPC *grpc.Server) {
-	ssov1.RegisterAuthServer(gRPC, &serverAPI{})
+func Register(gRPC *grpc.Server, auth Auth) {
+	ssov1.RegisterAuthServer(gRPC, &serverAPI{auth: auth})
 }
 
 func (s *serverAPI) Login(
 	ctx context.Context,
 	req *ssov1.LoginRequest,
 ) (*ssov1.LoginResponse, error) {
-	panic("implement this")
+	if err := validator.New().Struct(req); err != nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	token, err := s.auth.Login(ctx, req.GetEmail(), req.GetPassword(), int(req.GetAppId()))
+	if err != nil {
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &ssov1.LoginResponse{
+		Token: token,
+	}, nil
+
 }
 
 func (s *serverAPI) Register(
 	ctx context.Context,
 	req *ssov1.RegisterRequest,
 ) (*ssov1.RegisterResponse, error) {
-	panic("implement this")
+	if err := validator.New().Struct(req); err != nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	userID, err := s.auth.RegisterNewUser(ctx, req.GetEmail(), req.GetPassword())
+	if err != nil {
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &ssov1.RegisterResponse{
+		UserId: userID,
+	}, nil
 }
 
 func (s *serverAPI) IsAdmin(
 	ctx context.Context,
-	request *ssov1.IsAdminRequest,
+	req *ssov1.IsAdminRequest,
 ) (*ssov1.IsAdminResponse, error) {
-	panic("implement this")
+	if err := validator.New().Struct(req); err != nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	isAdmin, err := s.auth.IsAdmin(ctx, int64(req.GetUserId()))
+	if err != nil {
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &ssov1.IsAdminResponse{
+		IsAdmin: isAdmin,
+	}, nil
 }
