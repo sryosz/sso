456be16421f22356a0012c16b84cf26b06824790
srymad <zasd4478@gmail.com>
2024-07-10 20:10:26 +0300

    server base implementation

---
+++ internal/grpc/auth/server.go
@@ -0,0 +1,36 @@
+package auth
+
+import (
+	"context"
+	ssov1 "github.com/sryosz/protos/gen/go/sso"
+	"google.golang.org/grpc"
+)
+
+type serverAPI struct {
+	ssov1.UnimplementedAuthServer
+}
+
+func Register(gRPC *grpc.Server) {
+	ssov1.RegisterAuthServer(gRPC, &serverAPI{})
+}
+
+func (s *serverAPI) Login(
+	ctx context.Context,
+	req *ssov1.LoginRequest,
+) (*ssov1.LoginResponse, error) {
+	panic("implement this")
+}
+
+func (s *serverAPI) Register(
+	ctx context.Context,
+	req *ssov1.RegisterRequest,
+) (*ssov1.RegisterResponse, error) {
+	panic("implement this")
+}
+
+func (s *serverAPI) IsAdmin(
+	ctx context.Context,
+	request *ssov1.IsAdminRequest,
+) (*ssov1.IsAdminResponse, error) {
+	panic("implement this")
+}
