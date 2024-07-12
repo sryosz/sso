60f9c020b3ea172028db0c39d901eb626fc16329
srymad <zasd4478@gmail.com>
2024-07-12 17:21:01 +0300

    function for executing migration scripts

---
+++ cmd/migrator/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	dbURL := os.Getenv("DATABASE_URL")
+	migrationsPath := os.Getenv("MIGRATIONS_PATH")
+
+	if dbURL == "" {
+		panic("dbURL is required")
+	}
+	if migrationsPath == "" {
+		panic("migrations-path is required")
+	}
+
+	m, err := migrate.New(
+		migrationsPath,
+		dbURL,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := m.Up(); err != nil {
+		if errors.Is(err, migrate.ErrNoChange) {
+			fmt.Println("no migrations to apply")
+
+			return
+		}
+
+		panic(err)
+	}
+
+	fmt.Println("migrations applied")
+}
